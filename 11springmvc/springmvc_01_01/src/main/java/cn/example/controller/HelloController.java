f42bd3ca85c81a6178cc7e30afce5a09f7dfa773
sxj <841840692@qq.com>
2019-11-05 01:43:03 +0800

    springmvc helloword

---
+++ 11springmvc/springmvc_01_01/src/main/java/cn/example/controller/HelloController.java
@@ -0,0 +1,15 @@
+package cn.example.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(path="/hello")
+    public String sayHello(){
+        System.out.println("hello springmvc");
+        return "success";
+    }
+}
---
+++ 11springmvc/springmvc_01_01/src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xijun
+  Date: 2019/11/5
+  Time: 0:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h3>入门程序</h3>
+
+    <a href="hello">入门程序</a>
+
+
+</body>
+</html>
